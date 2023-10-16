0a6474f7a5bc69ed5bba4c0d4a57db58643165fb
Luis Alvarez <lealvarezp@unac.edu.co>
2023-10-16 03:06:33 -0300

    changes on db conection

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
+import 'dotenv/config';
 import { Server } from './src/models/Server';
-
 const server = new Server();
 
 server.init();
--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,14 +1,13 @@
-import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { Entites } from '../entities';
 
 export const AppDataSource = new DataSource({
     type: 'mariadb',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'root',
-    database: 'typeorm-test',
+    host: process.env.DB_URL,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: 'employees-erp',
     logging: true,
     entities: Entites,
     synchronize: true,
